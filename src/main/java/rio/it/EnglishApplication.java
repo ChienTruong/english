9094a24c59508412ce787e08ef033a7b9b39707b
ChienTruong <qchien2400@gmail.com>
2018-04-02 21:07:08 +0700

    Chien:: First Commit

---
+++ src/main/java/rio/it/EnglishApplication.java
@@ -0,0 +1,12 @@
+package rio.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishApplication.class, args);
+	}
+}
