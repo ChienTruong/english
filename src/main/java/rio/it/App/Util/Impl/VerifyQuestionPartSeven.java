dc93048b345f41f32c87e303a39df3798a4f2550
ngocson18081195 <ngocson18081195@gmail.com>
2018-04-16 20:39:30 +0700

    Son: done verifyPartSeven

--- src/main/java/rio/it/App/Util/Impl/VerifyQuestionPartSeven.java
+++ src/main/java/rio/it/App/Util/Impl/VerifyQuestionPartSeven.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rio.it.App.Dto.PartQuestionDto;
 import rio.it.App.Dto.QuestionDto;
+import rio.it.App.Dto.SubQuestionDto;
 import rio.it.App.Util.VerifyPartQuestion;
 
 import java.util.List;
@@ -16,18 +17,30 @@ public class VerifyQuestionPartSeven implements VerifyPartQuestion {
     @Override
     public boolean verify(PartQuestionDto partQuestionDto) {
         logger.info("Begin Verify Part Seven with condition: "+partQuestionDto);
-        if (!partQuestionDto.getQuestionDtoList().isEmpty()){
-            for (QuestionDto questionDto : partQuestionDto.getQuestionDtoList()){
-                if (questionDto.getFileImageDtoList() == null){
-                    System.out.println("Sai");
-                    break;
+        if (partQuestionDto.getPathFileMp3() == null){
+            if (!partQuestionDto.getQuestionDtoList().isEmpty()){
+                for (QuestionDto questionDto : partQuestionDto.getQuestionDtoList()){
+                    System.out.println(questionDto.getFileImageDtoList().size());
+                    if (questionDto.getFileImageDtoList().size() < 0){
+                        System.out.println("Hinh ko co");
+                        return false;
+                    }
+                    System.out.println(questionDto.getSubQuestionDtoList().size());
+                    if (questionDto.getSubQuestionDtoList().size() <2 ){
+                        System.out.println("Khong du subQuestion");
+                        return false;
+                    }
+                    for (SubQuestionDto subQuestionDto : questionDto.getSubQuestionDtoList()){
+                        System.out.println(subQuestionDto.getSentenceDtoList().size());
+                        if (subQuestionDto.getSentenceDtoList().size() != 4){
+                            System.out.println("ko du 4 cau hoi");
+                            return false;
+                        }
+                    }
+                    return true;
                 }
-
             }
-            return true;
         }
-
-        System.out.println("WWWW");
         return false;
     }
 }
